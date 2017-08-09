e67cf89203178afa95f93fe87fddad18c631cfb8
sridhar1910 <nani6006@gmail.com>
2017-08-09 12:08:59 -0500

    Initial commit

---
+++ src/HelloJava.java
@@ -0,0 +1,25 @@
+/**
+ * Created by theji on 6/6/2017.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        boolean gameOver = false;
+        int myScore = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+         if(gameOver == false){
+             int finalScore = myScore + (levelCompleted * bonus);
+            System.out.println("Total was " + finalScore);
+        }
+        else if(gameOver = true) {
+             System.out.println(myScore);
+         }
+
+
+
+
+
+
+    }
+}
